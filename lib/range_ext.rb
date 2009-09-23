819b02a96c42014fa850c87b2b7258e204afcfd7
Jack Chen <chendo@chendo.net>
2009-09-24 08:10:56 +1000

    added performance optimisations for Integer, Date and Time

--- lib/range_ext.rb
+++ lib/range_ext.rb
@@ -1,6 +1,20 @@
 class Range
   # TODO: make this be a no-op for Time/Dates
   warn_define :rand do
-    to_a.rand # not optimized, but will work regardless of range time
+    case first
+    when Integer
+      Kernel.rand((exclude_end? ? last - 1 : last) - first + 1) + first
+    when Date
+      first + Kernel.rand((exclude_end? ? last - 1 : last) - first + 1)
+    when Time
+      first + Kernel.rand((exclude_end? ? last - 1 : last) - first + 1)
+    else
+      @@rand_warnings ||= {}
+      if !@@rand_warnings[first.class.to_s.to_sym]
+        $stderr.puts "Falling back to non-optimised Range#rand for unimplemented class #{first.class.to_s}"
+        @@rand_warnings[first.class.to_s.to_sym] = true
+      end
+      to_a.rand
+    end
   end
 end
